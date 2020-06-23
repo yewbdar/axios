64c55396260d22e733382d8736153488ba0dd9a1
yewbdar <Yewbdar.Grima@Gmail.com>
2020-06-23 16:27:53 -0500

    get and post axios

--- axios.js
+++ axios.js
@@ -1,23 +1,47 @@
 // Performing a GET request
-axios.get('https://.../' + username)
-  .then(function(response){
-    console.log(response.data); // ex.: { user: 'Your User'}
-    console.log(response.status); // ex.: 200
-  });  
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+async function getUser() {
+    try {
+      const response = await axios.get('/user?ID=12345');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 // Requests will be executed in parallel...
 axios.all([
-    axios.get('https://...'),
-    axios.get('https://...')])
-         .then(axios.spread(function (userResponse, reposResponse) {
+    axios.get('https://user...'),
+    axios.get('https://Permissions...')])
+         .then(axios.spread(function (userResponse, permissResponse) {
     //... but this callback will be executed only when both requests are complete.
     console.log('User', userResponse.data);
-    console.log('Repositories', reposResponse.data);
+    console.log('Repositories', permissResponse.data);
   }));
 
 
 // Performing a POST request
-axios.post('/save', { firstName: 'Marlon', lastName: 'Bernardes' })
-  .then(function(response){
-    console.log('saved successfully')
-  });  
+axios.post('/user', {
+    firstName: 'yo',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
